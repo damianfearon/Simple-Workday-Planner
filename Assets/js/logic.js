408dd00094eed9dfca32f8da3ab2eb113e7c7c4f
unknown <damianfearon@hotmail.com>
2023-01-22 17:21:38 +0000

    add jquery to css

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,53 +1,121 @@
-var currentDate = moment().format('dddd MMMM Do, YYYY');
-var currentHour = parseInt(moment().format('H'));
 
-//Array that will go to local storage
-var savedEvents = ["", "", "", "", "", "", "", "", "" ];
+var currentDay = $("#currentDay");
+var timeBlocks = $(".container");
 
-$('#currentDay').text(currentDate);
-render();
-
-//Checks current time and changes background colors
-for (var i = 9; i < 18; i++){
-    if (i > currentHour){
-        $('#hour-'+ i).css("background-color", "#77dd77");
-    } else if(i === currentHour){
-        $('#hour-'+ i).css("background-color", "#ff6961");
-    } else if(i < currentHour){
-        $('#hour-'+ i).css("background-color", "#d3d3d3");
-    }
+function showTime() {
+  var now = moment().format("DD MMM YYYY [at] hh:mm:ss a");
+  currentDay.text(now);
 }
 
-//Accesses local storage and if it is not empty, it re-defines the savedEvents array
-function render(){
-    if (localStorage.getItem('Saved events') === null){
-
-    } else {
-        savedEvents = localStorage.getItem('Saved events').split(',');
-    }
-
-//Aoops through each textarea and assigns its value as the corresponding array item
-    for (var i = 0; i < savedEvents.length; i++){
-       var textContent = $('#hour-' + (9+i)).find("textarea");
-        textContent.val(savedEvents[i]);
-    }
+function hourOfWorkingDay() {
+  var hour = moment().hour();
+  return hour;
+ 
 }
 
-//Assigns a value from the targeted textarea to its corresponding spot in the array, then saves the array to localstorage
-$('.saveBtn').on('click', function(){
-    var targetedText = $(this).siblings()[1].value;
-    var whereToGo = parseInt($(this).parent().attr('id')[5] + $(this).parent().attr('id')[6]);
-    savedEvents[whereToGo - 9] = targetedText;
-    localStorage.setItem('Saved events', savedEvents);
-    render();
-})
 
-$('#clear-button').on('click', function(){
-    var clear = confirm('Are you sure you want to clear your calendar?');
-    if (clear === true){
-        console.log('Button clicked')
-        savedEvents = ["", "", "", "", "", "", "", "", "" ];
-        localStorage.setItem("Saved events", savedEvents);
-        render();
-        }
-    })
+var workingHours = [
+  [9, "am"],
+  [10, "am"],
+  [11, "am"],
+  [12, "pm"],
+  [13, "pm"],
+  [14, "pm"],
+  [15, "pm"],
+  [16, "pm"],
+  [17, "pm"],
+];
+
+// Function to convert in 12 hours 
+function amPm(index) {
+  if (workingHours[index][0] <= 12) {
+    return workingHours[index][0] + " " + workingHours[index][1];
+  }
+  if (workingHours[index][0] > 12) {
+    return workingHours[index][0] - 12 + " " + workingHours[index][1];
+  }
+}
+
+var timeRow;
+
+function setTimeBlocks() {
+  for (var i = 0; i < workingHours.length; i++) {
+    timeRow = $(`<div id="id-${i}" class="row">
+      </div>`);
+
+    if (workingHours[i][0] < hourOfWorkingDay()) {
+      timeRow.append(
+        $(`<div class="col-2 hour"><h4> ${amPm(i)}</h4></div>`),
+        $('<textarea class="col-8 description past">'),
+        $(`
+        <button class="col-2 saveBtn">
+          <i class="fas fa-save"></i>
+        </button>
+          `)
+      );
+    }
+    if (workingHours[i][0] === hourOfWorkingDay()) {
+      timeRow.append(
+        $(`<div class="col-2 hour"><h4>${amPm(i)}</h4></div>`),
+        $('<textarea class="col-8 description present">'),
+        $(`
+        <button class="col-2 saveBtn">
+          <i class="fas fa-save"></i>
+        </button>
+          `)
+      );
+    }
+    if (workingHours[i][0] > hourOfWorkingDay()) {
+      timeRow.append(
+        $(`<div class="col-2 hour"><h4>${amPm(i)}</h4></div>`),
+        $('<textarea class="col-8 description future">'),
+        $(`
+        <button class="col-2 saveBtn">
+          <i class="fas fa-save"></i>
+        </button>
+          `)
+      );
+    }
+    timeBlocks.append(timeRow);
+  }
+}
+
+setTimeBlocks();
+hourOfWorkingDay();
+
+var dailyTasks = {};
+
+function storeTasks() {
+  localStorage.setItem("tasks", JSON.stringify(dailyTasks));
+}
+
+function persistEvents() {
+  var storage = JSON.parse(localStorage.getItem("tasks"));
+
+  for (var task in storage) {
+    dailyTasks[task] = storage[task];
+    $(`#${task}`).children("textarea").text(`${storage[task]}`);
+  }
+}
+
+persistEvents();
+
+var saveBtn = $("button");
+
+
+saveBtn.click(function () {
+  var rowId = $(this).parent().attr("id");
+  var textareaValue = $(`#${rowId}`).children("textarea").val();
+
+  dailyTasks[rowId] = textareaValue;
+
+  storeTasks();
+});
+
+
+setInterval(showTime, 1000);
+
+
+setTimeout(function () {
+  location.reload();
+}, 1000 * (60 - moment().minutes()));
